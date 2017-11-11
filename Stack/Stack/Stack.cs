f9d3c4cc406ed1693fe66c9c849cbe2620a2e8eb
Alexandre Rosa <alexandre@Alexandres-MacBook-Pro.local>
2017-11-11 15:38:31 -0200

    atualização do repositório

--- Stack/Stack/Stack.cs
+++ Stack/Stack/Stack.cs
@@ -2,45 +2,46 @@
 using System.Collections;
 namespace Stack
 {
-    public class Stack
-    {
-        //private int[] elementos;
-        private ArrayList lista;
-        private int topo;
+    /**
+     * Implementação de uma pilha simples usando vetores para armazenar números.
+     * Este mesmo exemplo poderá ser implementado usando Objetos ou GameObjects.
+     */
 
-        public Stack()
-        {
-            lista = new ArrayList();
-            //elementos = new int[10];
+    public class Stack{
+        
+        private int[] elementos;
+        private int topo;
+        /*
+         * Construtor da classe. Nele instanciamos o nosso array (vetor) com 20 elementos (números).
+         */
+        public Stack(){
+            elementos = new int[20];
             topo = -1;
         }
 
         public void push(int elemento){
             if (!isFull()){
                 topo++;
-                lista.Add(elemento);
-                //elementos[topo] = elemento;
+                elementos[topo] = elemento;
             }
         }
 
         public int pop(){
             int e;
-            //e = elementos[topo];
-            e = (int)lista[topo];
+            e = elementos[topo];
             topo--;
             return e;
         }
 
         public int top(){
-            //return elementos[topo];
-            return lista.Count;
+            return elementos[topo];
         }
         public bool isEmpty(){
             return (topo == -1);
         }
 
         public bool isFull(){
-            return (topo == 9);
+            return (topo == 19);
         }
 
     }
